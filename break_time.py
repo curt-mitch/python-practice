df91209a5c523fee3d4dc3efb9a5f8c4f7b329af
curt-mitch <curtis.l.mitchell@gmail.com>
2014-06-25 10:43:56 -0700

    Original Python syntax practice.

---
+++ break_time.py
@@ -0,0 +1,10 @@
+import webbrowser
+import time
+
+total_breaks = 3
+break_count = 0
+
+while (break_count < total_breaks):
+	time.sleep(10)
+	webbrowser.open("http://www.youtube.com/watch?v=GaoLU6zKaws")
+	break_count += 1
